18c0cf54f57e8fca55eb40b62f7f4e13406c2dbb
Timothy Tas <ttas@c1r8p2.42nice.fr>
2024-08-09 09:10:25 +0200

    update

--- Cursus/Cercle 2/So_Long/srcs/Parsing/Parsing_Map.c
+++ Cursus/Cercle 2/So_Long/srcs/Parsing/Parsing_Map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parsing_Map.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 09:43:10 by ttas              #+#    #+#             */
-/*   Updated: 2024/08/05 17:21:17 by marvin           ###   ########.fr       */
+/*   Updated: 2024/08/09 09:09:16 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	parsing_map_size(t_map *map)
 {
-	char *buffer;
-	unsigned int i;
-	unsigned int j;
+	char			*buffer;
+	unsigned int	i;
+	unsigned int	j;
+
 	j = 0;
 	buffer = get_next_line(map->fd_map);
 	i = ft_strlen(buffer);
@@ -28,6 +29,7 @@ int	parsing_map_size(t_map *map)
 			return (INVALID_MAP_SIZE);
 		}
 		free(buffer);
+		map->map[j] = buffer;
 		buffer = get_next_line(map->fd_map);
 		j++;
 	}
@@ -37,4 +39,9 @@ int	parsing_map_size(t_map *map)
 	map->x = i;
 	map->y = j;
 	return (VALID_MAP_SIZE);
+}
+
+int	parsing_map_collectibles(t_map *map)
+{
+	
 }
