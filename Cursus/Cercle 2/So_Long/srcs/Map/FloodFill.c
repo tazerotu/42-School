87399a5777e74173df5212461c184824c04e9a99
Timothy Tas <ttas@c2r11p2.42nice.fr>
2024-11-12 10:53:49 +0100

    Added Path check

--- Cursus/Cercle 2/So_Long/srcs/Map/FloodFill.c
+++ Cursus/Cercle 2/So_Long/srcs/Map/FloodFill.c
@@ -6,7 +6,7 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 09:06:32 by ttas              #+#    #+#             */
-/*   Updated: 2024/11/12 10:29:47 by ttas             ###   ########.fr       */
+/*   Updated: 2024/11/12 10:53:15 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 static int parsing_exit_path(t_map *map, int x, int y)
 {
-	if(overflow(map, x, y) == -1 || map->floodfill[y][x] == 'F')
-		return 0;
-	if(map->floodfill[y][x] == '0' || map->floodfill[y][x] == 'C')
-		map->floodfill[y][x] = 'F';
+	int i;
+	i = 0;
+	if(overflow(map, x, y) == -1 || map->floodfill[y][x] == 'F' || map->floodfill[y][x] == '1')
+		return (0);
 	if(map->floodfill[y][x] == 'P')
 		return (1);
-	parsing_exit_path(map, x + 1, y);
-	parsing_exit_path(map, x - 1, y);
-	parsing_exit_path(map, x, y + 1);
-	parsing_exit_path(map, x, y - 1);
-	return 0;
+	if(map->floodfill[y][x] == '0' || map->floodfill[y][x] == 'C')
+		map->floodfill[y][x] = 'F';
+	i += parsing_exit_path(map, x + 1, y);
+	i += parsing_exit_path(map, x - 1, y);
+	i += parsing_exit_path(map, x, y + 1);
+	i += parsing_exit_path(map, x, y - 1);
+	return (i);
 }
 
 void parsing_map_exit_path(t_map *map)
--- Cursus/Cercle 2/So_Long/srcs/utils/Error.c
+++ Cursus/Cercle 2/So_Long/srcs/utils/Error.c
@@ -6,7 +6,7 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 10:57:48 by ttas              #+#    #+#             */
-/*   Updated: 2024/09/24 09:04:37 by ttas             ###   ########.fr       */
+/*   Updated: 2024/11/12 10:42:25 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	error_message(int error)
 {
-	ft_putstr_fd("Error code : ", ERROR_FD);
+	ft_putstr_fd("Error : ", ERROR_FD);
 	ft_putnbr_fd(error, ERROR_FD);
 	ft_putstr_fd("\n", ERROR_FD);
 	exit(1);
