f1ca37fe1aee7316455c8a6fc21c77e6b476f454
Timothy Tas <ttas@c2r12p1.42nice.fr>
2024-11-19 12:59:34 +0100

    checker

--- Cursus/Cercle 2/Pipex/V1/test.sh
+++ Cursus/Cercle 2/Pipex/V1/test.sh
@@ -10,5 +10,8 @@ printf "\nCommande effectuer : ./pipex test.txt cat 'wc -l' test2.txt\n"
 printf "Equivalent bash : cat test.txt | wc -l > test2.txt\n"
 printf "\nContenu du ficher test.txt :\n"
 cat test.txt
-printf "\n\nResultat du programme :\n"
+printf "\nResultat du programme :\n"
 cat test2.txt
+
+printf "\nReference :\n"
+cat test.txt | wc -l
