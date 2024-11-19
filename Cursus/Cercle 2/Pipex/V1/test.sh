c3f4b5ea7ad1201eb9d091db5e14494d99fcac02
Timothy Tas <ttas@c2r12p1.42nice.fr>
2024-11-19 11:09:08 +0100

    testeur

--- Cursus/Cercle 2/Pipex/V1/test.sh
+++ Cursus/Cercle 2/Pipex/V1/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf test.txt
+printf "Ecrivez le contenu de votre fichier d'entree\n"
+printf "Une fois terminer utiliser ctrl + d\n"
+cat > test.txt
 make re
 ./pipex test.txt cat 'wc -l' test2.txt
 printf "\nCommande effectuer : ./pipex test.txt cat 'wc -l' test2.txt\n"
