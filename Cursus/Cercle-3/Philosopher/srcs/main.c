9af80119bf535a8dc74fe11dfe28435c4c682db8
Timothy Tas <ttas@c2r11p1.42nice.fr>
2024-11-20 09:45:44 +0100

    update

--- Cursus/Cercle-3/Philosopher/srcs/main.c
+++ Cursus/Cercle-3/Philosopher/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 10:27:15 by ttas              #+#    #+#             */
-/*   Updated: 2024/11/20 09:44:20 by ttas             ###   ########.fr       */
+/*   Updated: 2024/11/20 09:45:24 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	main(int argc, char **argv)
 {
+	t_philo *philo;
+	
 	if (argc != 4)
 		error(ERROR_INPUT);
-	else
-		printf("%s", argv[0]);
-	return (0);
+	init_philosopher(philo, argc, argv);
+	return(0);
 }
