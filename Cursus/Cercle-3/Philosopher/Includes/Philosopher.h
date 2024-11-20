a5dbabf056c685ef192c6371d0365fbb7d14bbd4
Timothy Tas <ttas@c2r11p1.42nice.fr>
2024-11-20 09:16:38 +0100

    update

--- Cursus/Cercle-3/Philosopher/Includes/Philosopher.h
+++ Cursus/Cercle-3/Philosopher/Includes/Philosopher.h
@@ -6,7 +6,23 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 10:27:12 by ttas              #+#    #+#             */
-/*   Updated: 2024/11/20 08:59:51 by ttas             ###   ########.fr       */
+/*   Updated: 2024/11/20 09:12:49 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+// Includes
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+
+// Functions
+
+
+# endif
