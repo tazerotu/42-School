3cac716e7a23c9b81475650821ca0a8b37fc0f4b
Timothy Tas <ttas@c1r6p1.42nice.fr>
2024-12-03 09:56:01 +0100

    update

--- Cursus/Cercle-1/ft_printf/test_printf.sh
+++ Cursus/Cercle-1/ft_printf/test_printf.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
 
 make re
-echo "int main(void) " > main.c
-echo "{" >> main.c
-echo "	int	i;" >> main.c
-echo "	i = 0;" >> main.c
-echo "	i = ft_printf("'"%s%d", "ceci est un nombre de test : "'", -123456789);" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = ft_printf("'"ceci est un nombre de test : %i"'", -123456789);" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = ft_printf("'"ceci est un nombre de test : %u"'", -123456789);" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = ft_printf("'"ceci est un nombre Hexa de test : %x"'", -123456789);" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = ft_printf("'"ceci est un nombre Hexa de test : %X"'", -123456789);" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = ft_printf("'"pointeur :%p"'", "'"NULL"'");" >> main.c
-echo "	ft_printf("'"	|	Return : %d\n\n"'", i);'" >> main.c
-echo "	printf("'"Ceci est la reference :\n"'");" >> main.c
-echo "	i = printf("'"%s%d:"'", "'"ceci est un nombre de test : "'", -123456789);" >> main.c
-echo "	printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = printf("'"ceci est un nombre de test : %i"'", -123456789);" >> main.c
-echo "	printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = printf("'"ceci est un nombre de test : %u"'", -123456789);" >> main.c
-echo "	printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	i = printf("'"ceci est un nombre Hexa de test : %x"'", -123456789);" >> main.c
-echo "	printf("'	|	Return : %d\n'", i);" >> main.c
-echo "	i = printf("'ceci est un nombre Hexa de test : %X'", -123456789);" >> main.c
-echo "	printf("'	|	Return : %d\n'", i);" >> main.c
-echo "	i = printf("'"pointeur :%p"'", "'"NULL"'");" >> main.c
-echo "	printf("'"	|	Return : %d\n"'", i);" >> main.c
-echo "	return (0);" >> main.c
-echo "}" >> main.c
-
-cc main.c libftprintf.a
-
-
+git clone https://github.com/tazerotu/42-Test.git
+clear
+grep -R "Created:" -A 1 ./**
+printf "\n\n\n"
+cc ./42-Test/Cercle-1/Printf/Printf-Main.c libftprintf.a
+rm -rf 42-Test
+./a.out
