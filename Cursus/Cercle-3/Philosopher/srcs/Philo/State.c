6a39b5666c789228eb69dcd8a5d5879595339f68
Timothy Tas <ttas@c2r9p2.42nice.fr>
2025-01-07 09:41:44 +0100

    added state message

---
+++ Cursus/Cercle-3/Philosopher/srcs/Philo/State.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   State.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 09:25:39 by ttas              #+#    #+#             */
+/*   Updated: 2025/01/07 09:38:24 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../Includes/Philosopher.h"
+
+void state(t_philo philo, long int time)
+{
+	struct timeval  tv;
+	gettimeofday(&tv, NULL);
+	double time_in_mill = ((tv.tv_sec) * 1000 + (tv.tv_usec) / 1000) - time;
+	printf("%.0f : ", time_in_mill);
+	if(philo.is_eating)
+		printf("Philo %d, is eating\n", philo.n);
+	else if(philo.is_thinking)
+		printf("Philo %d, is thinking\n", philo.n);
+	else if(philo.is_sleeping)
+		printf("Philo %d, is sleeping\n", philo.n);
+	else if(philo.is_dead)
+		printf("Philo %d, is dead\n", philo.n);
+}
