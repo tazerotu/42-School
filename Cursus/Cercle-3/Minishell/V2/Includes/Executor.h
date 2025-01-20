fb0158f70906c62aa4b6150a1b043948564b3116
Timothy Tas <ttas@c2r4p9.42nice.fr>
2025-01-20 11:53:51 +0100

    Update Executor

--- Cursus/Cercle-3/Minishell/V2/Includes/Executor.h
+++ Cursus/Cercle-3/Minishell/V2/Includes/Executor.h
@@ -6,7 +6,7 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 11:15:50 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/20 11:24:28 by ttas             ###   ########.fr       */
+/*   Updated: 2025/01/20 11:52:23 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,27 +21,23 @@
 # include <readline/history.h>
 
 // Struct
-typedef struct s_cmd
-{
-	char *cmd;
-	char *args;
-}	t_cmd;
-
-typedef struct s_pipe
-{
-	int input;
-	int output;
-	int err_output;
-	struct s_cmd cmd;
-}	t_pipe;
-
 typedef struct s_splice
 {
 	int length;
 	char *str;
 }	t_splice;
 
-int tokenizer(char *input);
-int quote(char *input, int start);
+typedef struct s_pipe
+{
+	int input;
+	int output;
+	int err_output;
+	struct s_splice *cmd;
+	char **envp;
+}	t_pipe;
+
+// Functions
+void init(char *str);
+void splicer(char *str);
 
 #endif
