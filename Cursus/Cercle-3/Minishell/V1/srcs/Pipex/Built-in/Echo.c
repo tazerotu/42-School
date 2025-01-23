35c529eb9f78918cdcbd8c6e082351b3cd50a08b
Timothy Tas <ttas@c2r5p1.42nice.fr>
2025-01-23 10:48:07 +0100

    update

--- Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Echo.c
+++ Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Echo.c
@@ -6,19 +6,40 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:04:56 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/23 10:31:13 by ttas             ###   ########.fr       */
+/*   Updated: 2025/01/23 10:48:03 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../Includes/Executor.h"
 
-char **parsing_echo(char *str)
+static char **parsing_echo(char *str)
 {
+	int i;
+	int j;
 	char **echo;
+	if(ft_strnstr(str, "-n", 2))
+	{
+		i = 2;
+		j = 0;
+		echo = malloc(2 * sizeof(char *));
+		echo[0] == "-n";
+		while(str[i])
+		{
+			echo[1][j] = str[i];	
+			i++;
+			j++;
+		}
+	}
+	else
+	{
+		echo = malloc(2 * sizeof(char *));
+		echo[0] = "\0";
+		echo[1] = ft_strdup(*str + 2);
+	}
 	return (echo);
 }
 
-void bi_echo(char *str, int fd)
+void bi_echo(char *str)
 {
 	int i;
 	i = 0;
@@ -27,7 +48,7 @@ void bi_echo(char *str, int fd)
 	if(!fd)
 		fd = 1;
 	if(echo[0] != "-n")
-		ft_fprintf(fd, "%s\n", echo[1]);
+		ft_printf("%s\n", echo[1]);
 	else
 	{
 		while(echo[1][i])
@@ -35,9 +56,12 @@ void bi_echo(char *str, int fd)
 			if(echo[1][i] == "\n")
 			{
 				if(echo[1][i+1] != "\0")
-					ft_fprintf(fd, " ");
+					ft_printf(" ");
 				i++;
-			}	
+			}
+			ft_printf("%c", echo[1][i]);
+			i++;	
 		}
 	}
+	free(echo);
 }
