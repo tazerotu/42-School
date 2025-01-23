62189946ebbd43636eaf65a17de6137f9eb8f869
Timothy Tas <ttas@c2r5p1.42nice.fr>
2025-01-23 09:54:09 +0100

    update

--- Cursus/Cercle-3/Minishell/V2/Includes/Executor.h
+++ Cursus/Cercle-3/Minishell/V2/Includes/Executor.h
@@ -6,7 +6,7 @@
 /*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 11:15:50 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/20 14:20:05 by ttas             ###   ########.fr       */
+/*   Updated: 2025/01/23 09:53:54 by ttas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,18 @@ typedef struct s_splice
 	char *str;
 }	t_splice;
 
+typedef struct s_command
+{
+	char **redir_tok;
+	char **arg_tok;
+}	t_cmd;
+
 typedef struct s_pipe
 {
-	int input;
-	int here_doc;
-	int output;
-	int err_output;
 	int n_pipe;
 	int semicolon;
-	char **cmd;
 	char **envp;
+	t_cmd *cmd;
 }	t_pipe;
 
 // Functions
@@ -46,6 +48,7 @@ t_pipe init(t_pipe pipe, char *str, char **envp);
 
 	// Command initialization
 void verify_semicolon(void);
+void verify_cmd(void);
 void here_doc(void);
 void split(void);
 
@@ -54,6 +57,14 @@ void execute(void);
 void get_path(void);
 void child_process(void);
 void parent_process(void);
-
+// with option -n
+void echo(char *option, char *str);
+// with absolute or relative path
+void cd(char *path);
+void pwd(void);
+void export(void);
+void unset(void);
+void env(void);
+void exit(void);
 
 #endif
