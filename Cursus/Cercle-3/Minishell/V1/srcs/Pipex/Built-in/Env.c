0038a7878fcb3d6b55cbaae74df4ce614e05f29d
tazerotu <tazerotu@Desktop-Timothy>
2025-01-29 17:44:56 +0100

    finished PWD

--- Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Env.c
+++ Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Env.c
@@ -3,10 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:04:54 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/23 10:04:54 by ttas             ###   ########.fr       */
+/*   Updated: 2025/01/29 17:02:43 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../../Includes/Executor.h"
+
+void env_add_back(void)
+{
+
+}
+void env_add_front(void)
+{
+	
+}
+
+void env(char **envp)
+{
+	int i;
+	i = 0;
+	while(envp[i])
+	{
+		ft_printf("%s", envp[i]);
+		i++;
+	}
+}
--- Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Pwd.c
+++ Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Pwd.c
@@ -3,10 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   Pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:05:07 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/23 10:05:10 by ttas             ###   ########.fr       */
+/*   Updated: 2025/01/29 17:02:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../../Includes/Executor.h"
+
+char *pwd(void)
+{
+	char str[1000];
+	char *test;
+	test = getcwd(str, sizeof(str));
+	test = ft_strnstr(test, "Minishell");
+	int i = value;
+	while(test[i])
+		i++;
+	char *ret = malloc((i - value) * sizeof(char));
+	i = 0;
+	while(test[value])
+	{
+		ret[i] = test[value];
+		value++;
+		i++;
+	}
+	return(ret);
+}
+
+// int main(void)
+// {
+// 	char *str;
+// 	str = pwd();
+// 	printf("%s\n", str);
+// 	return 0;
+// }
