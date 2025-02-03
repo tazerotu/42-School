75923df6eb8f79d6ec41933c0c196aee9661f493
tazerotu <tazerotu@Desktop-Timothy>
2025-02-03 11:10:12 +0100

    PWD finished version (no output)

--- Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Pwd.c
+++ Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Pwd.c
@@ -6,36 +6,28 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:05:07 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/29 17:02:15 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/31 16:01:24 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../Includes/Executor.h"
 
-char *pwd(void)
+void bi_pwd(void)
 {
-	char str[1000];
-	char *test;
-	test = getcwd(str, sizeof(str));
-	test = ft_strnstr(test, "Minishell");
-	int i = value;
-	while(test[i])
-		i++;
-	char *ret = malloc((i - value) * sizeof(char));
-	i = 0;
-	while(test[value])
-	{
-		ret[i] = test[value];
-		value++;
-		i++;
-	}
-	return(ret);
+    // Get the current working directory
+    char *cwd = getcwd(NULL, 0);
+
+    // Find the "Minishell" substring in cwd
+    ft_printf("%s\n", ft_strnstr(cwd, "Minishell", ft_strlen(cwd)));
+
+    // No need to free str here since you're returning ret, and you won't use `str` anymore
+	free(cwd);
 }
 
 // int main(void)
 // {
 // 	char *str;
-// 	str = pwd();
+// 	str = bi_pwd();
 // 	printf("%s\n", str);
 // 	return 0;
 // }
