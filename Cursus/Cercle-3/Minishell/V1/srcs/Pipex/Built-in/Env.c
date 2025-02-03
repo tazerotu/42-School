d223b1d19f69e80c701c96e085346d66e9bee73f
tazerotu <tazerotu@Desktop-Timothy>
2025-02-03 11:10:38 +0100

    ENV finished version

--- Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Env.c
+++ Cursus/Cercle-3/Minishell/V1/srcs/Pipex/Built-in/Env.c
@@ -6,28 +6,20 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 10:04:54 by ttas              #+#    #+#             */
-/*   Updated: 2025/01/29 17:02:43 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/31 15:11:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../Includes/Executor.h"
 
-void env_add_back(void)
-{
-
-}
-void env_add_front(void)
-{
-	
-}
-
-void env(char **envp)
+void bi_env(t_pipe *pipex, char **envp)
 {
 	int i;
-	i = 0;
-	while(envp[i])
+	i = 1;
+	pipex->envp = env_new(envp[0]);
+	while (envp[i])
 	{
-		ft_printf("%s", envp[i]);
+		env_add_back(pipex->envp, env_new(envp[i]));
 		i++;
 	}
 }
