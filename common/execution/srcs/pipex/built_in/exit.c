f0fb876f30109d6ccb75db877d64cf0a36afb706
Timothy Tas <ttas@c1r8p1.42nice.fr>
2025-05-16 12:51:09 +0200

    update

---
+++ common/execution/srcs/pipex/built_in/exit.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttas <ttas@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 10:05:01 by ttas              #+#    #+#             */
+/*   Updated: 2025/05/16 12:44:32 by ttas             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../includes/executor.h"
+
+static int exit_args(char **cmd)
+{
+	int exit;
+	
+	exit = atoi(cmd[1]);
+	while(exit > 255)
+		exit -= 256;
+	return (exit);
+}
+
+int	bi_exit(t_pipe *pipe, char **cmd)
+{
+	int	i;
+	int	exit;
+
+	i = 0;
+	while (cmd[i])
+		i++;
+	if (i > 2)
+		ft_printf("Too many arguments");
+	i = 0;
+	if(cmd[1])
+	{
+		while (cmd[1][i])
+		{
+			if (ft_isdigit(cmd[1][i++]) == 0)
+			{
+				ft_printf("Non numerical argument");
+				pipe->exit = INVALID_CMD;
+				return (INVALID_CMD);
+			}
+		}
+		exit = exit_args(cmd);
+	}
+	else
+		exit = 1;
+	pipe->exit = exit;
+	return (exit);
+}
